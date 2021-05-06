d60ef42027fed5e472b8a36b2ce34232f3e625cb
Ángyán Márk <angyanmark97@gmail.com>
2021-05-06 16:22:47 +0200

    crash button 😈

--- app/src/main/java/hu/angyanmark/filmsapp/ui/about/AboutActivity.java
+++ app/src/main/java/hu/angyanmark/filmsapp/ui/about/AboutActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import hu.angyanmark.filmsapp.ui.list.ItemListActivity;
 import hu.angyanmark.filmsapp.R;
@@ -21,6 +22,11 @@ public class AboutActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        final Button button = findViewById(R.id.crash_button);
+        button.setOnClickListener(v -> {
+            throw new RuntimeException("crash");
+        });
 
         setTitle(getResources().getText(R.string.action_about));
     }
